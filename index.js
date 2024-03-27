7b1527f8497e0c97095e2270337a30b672ec5d2b
unknown <laothucatkhanhle@gmail.com>
2024-03-28 00:25:14 +0700

    Phuc hoi nhu cu

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 const express = require('express');
 const app = express();
-const cors = require("cors");
-const corsConfig = {
-  origin: "*",
-  credential: true,
-  methods: ["GET", "POST", "PUT", "DELETE"]
-};
-app.options("", cors(corsConfig));
-app.use(cors(corsConfig));
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
