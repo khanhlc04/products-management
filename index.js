02b192d3c5bda76bb6bfe59019e506d6a2f43867
unknown <laothucatkhanhle@gmail.com>
2024-03-29 00:35:18 +0700

    2

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ global._io = io;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set('views', './views');
+app.set("views", `${__dirname}/views`);
 app.set('view engine', 'pug');
-app.use(express.static('public'));
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
 
